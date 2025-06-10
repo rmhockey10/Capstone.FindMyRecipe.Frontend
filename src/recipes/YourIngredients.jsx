94ed4e0c0d9a757ff582846e3cedb9436e59e303
rmhockey10 <rees.m.hannah@gmail.com>
2025-06-10 12:53:37 -0400

    adding latest (I forgot if I made changes since last push)

--- src/recipes/YourIngredients.jsx
+++ src/recipes/YourIngredients.jsx
@@ -12,6 +12,7 @@ export default function YourIngredients() {
   const getRecipes = () => {
     const recipes = get(yourIngredients);
     setRecipes(recipes);
+    console.log(yourIngredients);
     navigate("/recipes");
   };
 
